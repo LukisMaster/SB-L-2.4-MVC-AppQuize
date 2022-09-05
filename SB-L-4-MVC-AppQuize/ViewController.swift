a509b3f8da4fdfc7f03489c84a4c3eccc3171ca2
LukisMaster <112563277+LukisMaster@users.noreply.github.com>
2022-09-05 16:22:30 +0300

    Initial Commit

---
+++ SB-L-4-MVC-AppQuize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB-L-4-MVC-AppQuize
+//
+//  Created by Sergey Nestroyniy on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
